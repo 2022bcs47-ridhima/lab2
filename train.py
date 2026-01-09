1126d6d90e2339cabc76e649dfa448091fdb1af2
Ridhima212215 <ridhimaedimadakala5571@gmail.com>
2026-01-09 15:54:28 +0530

    Model: Ridge, alpha=1.0, StandardScaler

--- train.py
+++ train.py
@@ -24,7 +24,9 @@ X_test = scaler.transform(X_test)
 
 # Train the Model
 
-model = LinearRegression()
+from sklearn.linear_model import Ridge
+model = Ridge(alpha=1.0)
+
 model.fit(X_train, y_train)
 
 # Evaluation Metrics
